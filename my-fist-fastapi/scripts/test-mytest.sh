2fe1443513d792eaeedb70af2a161d886081e3d1
Jonas Tsai <jonastsai@Jonass-MacBook-Pro.local>
2020-10-09 21:03:01 +0800

    Add test-mytest.sh

---
+++ my-fist-fastapi/scripts/test-mytest.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+#DOMAIN=test
+
+echo "mytest"
+echo $DOMAIN
+
+# test by DOMAIN=test sh test-mytest.sh
+# test by DOMAIN=test ./test-mytest.sh
